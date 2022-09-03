c18e7784f9250b6e034c7075f94fd8d422488fac
AnitaLuzardo <aniebratt@gmail.com>
2022-09-03 00:00:34 -0300

    modificaciones en navbar

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from "react";
 import './App.css';
 import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
+import Catalogo from "./pages/Catalogo";
+import Nosotros from "./pages/Nosotros"
 import Login from "./components/Login";
 import Register from "./components/Register";
 import {
@@ -17,6 +19,8 @@ function App() {
         <Navbar />
         <Routes>
           <Route exact path="/" element= {<Home />} />
+          <Route exact path="/catalogo" element= {<Catalogo />} />
+          <Route exact path="/" element= {<Nosotros />} />
           <Route exact path="/login" element= {<Login />} />
           <Route exact path="/register" element= {<Register />} />
         </Routes>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,32 +7,34 @@ import Cookie from '../images/cookies.png'
 
 function Navbar() {
   let enlace = [
-    {name: "Home", link:"/"},
-    {name: "Login", link:"/login"},
-    {name: "Register", link:"/register"}
+    {name: "Inicio", link:"/"},
+    {name: "Catalogo", link:"/catalogo"},
+    {name: "Nosotros", link:"/nosotros"}
   ]
 
   let [open, setOpen] = useState(false)
 
   return (
-    <div className="shadow-md w-full fixed top-0 left-0">
+    <div className="w-full fixed top-0 left-0">
       <div className="md:flex items-center justify-between bg-red-100 py-4 md:px-10 px-7">
         <div className='font-bold500 text-2xl cursor-pointer flex items-center 
         text-gray-800'>
-          Planet 
+          <h1 className="logo-planet">
+            Planet
             <span className='text-3x1 text-indigo-600 pt-2'>
               <img className='logo' src={Cookie} alt="" />
-            </span> 
-          Cookie
+            </span>
+            Cookie 
+          </h1>
         </div>
-        <div onClick={() => setOpen(!open)} className='text-3xl absolute right-8 top-6 cursor-pointer md:hidden'>
-          <ion-icon name="menu-outline"></ion-icon>
+        <div onClick={() => setOpen(!open)} className='text-xl absolute right-8 top-6 cursor-pointer md:hidden'>
+          <h2 className="menu-resp">Menu<box-icon type='solid' name='cookie'></box-icon></h2>
         </div>
         <ul className={`md:flex md:items-center md:pb-0 pb-12 pt-0 absolute md:static bg-red-100 md:z-auto z-[-1]
-        left-0 w-full md:w-auto md:pl-0 pl-9 transition-all duration-1000 ease-in ${open ? 'top-20': 'top-[-300px]'}`}>
+          left-0 w-full md:w-auto md:pl-0 pl-9 transition-all duration-1000 ease-in ${open ? 'top-20': 'top-[-300px]'}`}>
           {
             enlace.map((link)=>(
-              <li key={link.name} className='md:ml-8 text-xl md:my-0 my-7'>
+              <li key={link.name} className='md:ml-6 text-ms md:my-0 my-7'>
                 <Link to={link.link}>{link.name}</Link>
               </li>
             ))
