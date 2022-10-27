25a1f4d2a88caaeb67c9bf7fb19eef1cafad1164
AnitaLuzardo <aniebratt@gmail.com>
2022-10-27 18:15:05 -0300

    maquetado de vista Nosotros

--- src/pages/Nosotros.jsx
+++ src/pages/Nosotros.jsx
@@ -3,7 +3,14 @@ import "../stylesheets/Nosotros.css"
 
 function Nosotros() {
   return (
-    <h1 className='title_nosotros'>Nosotros</h1>
+    <div className='container_about'>
+      <h1 className='title_nosotros'>Nosotros</h1>
+      <div className='content_planet'>
+        <h2 className='oval-half-red'>PLANET COOKIE</h2>
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. 
+          Dicta necessitatibus ad architecto.</p>
+      </div>
+    </div>
   )
 }
 
