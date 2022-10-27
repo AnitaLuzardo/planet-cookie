28279491afe3c56723a860201dc57a726f09e76b
AnitaLuzardo <aniebratt@gmail.com>
2022-10-27 16:10:46 -0300

    finaliza maquetado de contactanos

--- src/components/contactForm/form.jsx
+++ src/components/contactForm/form.jsx
@@ -2,29 +2,27 @@ import React from 'react'
 
 function form() {
   return (
-    
-      <form className='content_form'>
-        <input type="text" 
-          name="name"
-          placeholder="Nombre"
-          className="contact_input"
-        />
-        <input type="email" 
-          name="email"
-          placeholder="Email"
-          className="contact_input"
-        />
-        <textarea 
-          type= "text"
-          name= "texto"
-          placeholder= "Escriba su mensaje aquí"
-          className="contact_input_text"
-        />
-        <button className="bg-indigo-600 text-white py-1 px-4 rounded hover:bg-indigo-400 duration-500 w-40 mt-8"> 
-          Enviar 
-        </button>
-      </form>
-  
+    <form className='content_form'>
+      <input type="text" 
+        name="name"
+        placeholder="Nombre"
+        className="contact_input"
+      />
+      <input type="email" 
+        name="email"
+        placeholder="Email"
+        className="contact_input"
+      />
+      <textarea 
+        type= "text"
+        name= "texto"
+        placeholder= "Escriba su mensaje aquí"
+        className="contact_input_text"
+      />
+      <button className="bg-indigo-600 text-white py-1 px-4 rounded hover:bg-indigo-400 duration-500 w-40 mt-4"> 
+        Enviar 
+      </button>
+    </form>
   )
 }
 
--- src/pages/Contactanos.jsx
+++ src/pages/Contactanos.jsx
@@ -5,8 +5,20 @@ import FormText from "../components/contactForm/form"
 function Contactanos() {
   return (
     <div className='container_contact'>
-      <h1 className='title_contact'>Contactanos</h1>
-      <FormText />
+      <div className='content_inf'>
+        <h1 className='title_contact'>Contactanos</h1>
+        <h3 className='contact_h3'>
+          Oye dejanos tu consulta!, 
+          tambien puedes visitar nuestras
+          redes.
+        </h3>
+        <p className='content_p'>Nuestros horarios de atención son de Lunes a Viernes de 08:30 a 18:30hs  
+          Sábados de 9:00 a 17:30hs
+        </p>
+      </div>
+      <div className='container_form_contact'>
+        <FormText />
+      </div>
     </div>
   )
 }
