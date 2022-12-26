3d80a17822d38314031a1a6d3c25f83f0ba9699e
AnitaLuzardo <aniebratt@gmail.com>
2022-12-26 16:18:02 -0300

    Validación de formulario contactanos

--- src/components/contactForm/form.jsx
+++ src/components/contactForm/form.jsx
@@ -1,50 +1,101 @@
 import React from 'react'
-import emailjs from '@emailjs/browser';
-// import { useState } from 'react';
+import { useForm } from './useForm';
 
-const form = () => {
-  // const initialData = {
-  //   name: '',
-  //   email: '',
-  //   texto: ''
-  // };
+const initialForm = {
+    name: '',
+    email: '',
+    texto: ''
+  };
 
-  const sendEmail = (event) => {
-    event.preventDefault();
+const validationsForm = (form) => {
+  let errors = {};
+  let regexName = /^[A-Za-zÑñÁáÉéÍíÓóÚúÜü\s]+$/;
+  let regexEmail = /^(\w+[/./-]?){1,}@[a-z]+[/.]\w{2,}$/;
+  let regexComments = /^.{1,255}$/;
 
-    console.log('enviando formulario')
+  if(!form.name.trim()){
+    errors.name = 'El campo "Nombre" es requerido'
+  } else if(!regexName.test(form.name.trim())) {
+    errors.name = 'El campo "Nombre" solo acepta letras y espacios en blanco'
+  };
 
-    emailjs.sendForm('service_uq5wzh8', 'template_rpghgif', event.target, 'OkvLQcE4Ih-vVw5yq')
-    .then(response => console.log('Respuesta', response))
-    .catch(error => console.log('ERROR', error))
+  if(!form.email.trim()){
+    errors.email = 'El campo "Email" es requerido'
+  } else if(!regexEmail.test(form.email.trim())) {
+    errors.email = 'El campo "Email" es inconrrecto'
   }
 
+  if(!form.texto.trim()){
+    errors.texto = 'El campo "Mensaje" es requerido'
+  } else if(!regexComments.test(form.texto.trim())){
+    errors.texto = 'El campo "Mensaje" debe tener solo 255 caracteres'
+  }
+
+  return errors;
+}
+
+const form = () => {
+  
+  const{
+    form, 
+    errors, 
+    response,
+    handleChange,
+    handleBlur,
+    handleSubmit
+    // eslint-disable-next-line react-hooks/rules-of-hooks
+  } = useForm(initialForm, validationsForm);
+
+  // const sendEmail = (event) => {
+  //   event.preventDefault();
+
+  //   console.log('enviando formulario')
+
+  //   emailjs.sendForm('service_uq5wzh8', 'template_rpghgif', event.target, 'OkvLQcE4Ih-vVw5yq')
+  //   .then(response => console.log('Respuesta', response))
+  //   .catch(error => console.log('ERROR', error))
+  // }
+
   return (
-    <form className='content_form' onSubmit={sendEmail}>
-      <input type="text" 
-        name="name"
-        placeholder="Nombre"
-        className="contact_input"
-        onBlur={(e) => console.log(e.target.value)}
-      />
-      {/* <div>
-        <p className=''>Debe colocar su Nombre!</p>
-      </div> */}
-      <input type="email" 
-        name="email"
-        placeholder="Email"
-        className="contact_input"
-      />
-      <textarea 
-        type= "text"
-        name= "texto"
-        placeholder= "Escriba su mensaje aquí"
-        className="contact_input_text"
-      />
-      <button className="bg-indigo-600 text-white py-1 px-4 rounded hover:bg-indigo-800 duration-500 w-40 mt-4"> 
-        Enviar 
-      </button>
-    </form>
+    <>
+      <form className='content_form' onSubmit={handleSubmit}>
+        <input type="text" 
+          name="name"
+          placeholder="Nombre"
+          className="contact_input"
+          onChange={handleChange}
+          onBlur={handleBlur}
+          value={form.name} 
+          required
+        />
+        {errors.name && <p className='errorsContact'>{errors.name}</p>}
+        <input type="email" 
+          name="email"
+          placeholder="Email"
+          className="contact_input"
+          onChange={handleChange}
+          onBlur={handleBlur}
+          value={form.email} 
+          required
+        />
+        {errors.email && <p className='errorsContact'>{errors.email}</p>}
+        <textarea 
+          type= "text"
+          name= "texto"
+          placeholder= "Escriba su mensaje aquí"
+          className="contact_input_text"
+          onChange={handleChange}
+          onBlur={handleBlur}
+          value={form.texto} 
+          required
+        />
+        {errors.texto && <p className='errorsContact'>{errors.texto}</p>}
+        <button className="bg-indigo-600 text-white py-1 px-4 rounded hover:bg-indigo-800 duration-500 w-40 mt-4 mb-6"> 
+          Enviar 
+        </button>
+      </form>
+      {response && <p className='exito'>Tu mensaje fue enviado con exito!!!</p>}
+    </>
   )
 }
 
--- src/components/contactForm/useForm.jsx
+++ src/components/contactForm/useForm.jsx
@@ -1,8 +1,53 @@
-import React, {useState} from "react"
+import {useState} from "react";
+import emailjs from '@emailjs/browser';
 
-export const useForm = (initialForm) => {
+export const useForm = (initialForm, validateForm) => {
   const [form, setForm] = useState(initialForm);
   const [errors, setErrors] = useState({});
-  const [loading, setLoading] = useState(false);
-  const [response, setResponse] = useState(null);
+  const [response, setResponse] = useState(null); // es quien guarda la respuesta
+
+  //Hook personalizado va retornar cualquier tipo de estructura de dato en JavaScript 
+  //en este caso retorna un objeto
+
+  const handleChange = (e) => {
+    const{name, value} = e.target
+    setForm({
+      ...form,
+      [name]: value //refactorizacion de codigo
+      // [e.target.name]: e.target.value
+    })
+  }; //para que detecte el cambio de los valores
+
+  const handleBlur = (e) => {
+    handleChange(e);
+    setErrors(validateForm(form))
+  }; //donde se hacen las validaciones que se muestran en el front
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setErrors(validateForm(form)); //Validamos que el formulario no tenga errores
+
+    if(Object.keys(errors).length === 0) {
+      emailjs.sendForm('service_uq5wzh8', 'template_rpghgif', e.target, 'OkvLQcE4Ih-vVw5yq')
+      .then(response => console.log('Respuesta', response))
+      .catch(error => console.log('ERROR', error))
+
+      .then(()=> {
+        setResponse(true);
+        setForm(initialForm);
+        setTimeout(()=> setResponse(false), 5000)
+      })
+    } else{
+      return;
+    }
+  }
+
+  return{
+    form, 
+    errors, 
+    response,
+    handleChange,
+    handleBlur,
+    handleSubmit
+  }
 }
