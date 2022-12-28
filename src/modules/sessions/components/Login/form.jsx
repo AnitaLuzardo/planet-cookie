ced5eca48a647b393ca526e1f1dcbb582b8901e4
AnitaLuzardo <aniebratt@gmail.com>
2022-12-28 18:14:22 -0300

    validaciones del Formulario de Registro

--- src/modules/sessions/components/Login/form.jsx
+++ src/modules/sessions/components/Login/form.jsx
@@ -1,67 +1,75 @@
 import React from 'react'
 import { useState } from 'react';
 import loginStyle from '../../styles/Login.module.css';
-// import loginApi from '../../Services/loginApi';
+
+const validateForm = (form) => {
+  let errors = {};
+  let regexEmail = /^(\w+[/./-]?){1,}@[a-z]+[/.]\w{2,}$/;
+ 
+  if(!form.email) {
+    errors.email = 'El campo "Email" es requerido'
+  } else if(!regexEmail.test(form.email.trim())) {
+    errors.email = 'El campo "Email" es inconrrecto'
+  }
+
+  if(!form.pwd) {
+    errors.pwd = 'El campo "Password" es requerido'
+  }
+
+  return errors;
+}
 
 function LoginForm({ login }) {
-
-  // const estado = useState(un valor cualquiera);
-  // estado = [null, callback];
-  // estado[0] = un valor cualquiera;
-  // estado[1] = callback;
-  // estado[1](otro valor cualquiera)
-  // estado[0] = otro valor cualquiera
-  // const [var1, var2] = [valor1, valor2, valor3, ..., valorN]; array[0]
+//==============================================================================
   const [dataLogin, setDataLogin] = useState({
     email: '',
     pwd: ''
   });
 
-//   const [error, setError] = useState(false);
-
-  const handleSubmit = e => { //evita el procesamiento automatico 
-    e.preventDefault();
-    
-    // const {email, pwd} = dataLogin;
-    // if(!email || !pwd){
-    //   // setError(true);
-    //   alert('Debe completar los campos');
-    //   return;
-    // }
-
-    login(dataLogin);
-  }
-
+  const [errors, setErrors] = useState({})
+//==============================================================================  
+  
   const handleChange = (e) => {
     // console.log('name', e.target.name, '- value', e.target.value);
+
+    const{name, value} = e.target
     setDataLogin({
       ...dataLogin,
-      [e.target.name]: e.target.value
+      [name]: value
+      // [e.target.name]: e.target.value
     });
   }
-
-//   const ViewError = () => (
-//     <p className="text-center m-4 text-red-500">Debe llenar todos los campos</p>
-//   ) 
+//============================================================================== 
+  const handleBlur = (e) => {
+    handleChange(e);
+    setErrors(validateForm(dataLogin))
+  };
+//==============================================================================
+  const handleSubmit = e => { //evita el procesamiento automatico 
+    e.preventDefault();
+    login(dataLogin);
+  }
+//=============================================================================
 
   return (
     <>
-      {/* {
-        error ? <ViewError /> : null
-      } */}
       <form className={loginStyle.container_form} onSubmit={handleSubmit} >
         <input type="email" 
           name="email"
           placeholder="Email"
           className={loginStyle.input_login}
           onChange={handleChange}
+          onBlur={handleBlur}
         />
+        {errors.email && <p className={loginStyle.errorsLogin}> {errors.email} </p>}
         <input type="password" 
           name="pwd"
           placeholder="Password"
           className={loginStyle.input_login}
           onChange={handleChange}
+          onBlur={handleBlur}
         /> 
+        {errors.pwd && <p className={`${loginStyle.errorsLogin} ${loginStyle.pwdStyle}`}>{errors.pwd}</p>}
         <button 
           type='submit'
           className="bg-indigo-600 text-white py-1 px-4 rounded hover:bg-indigo-800 duration-500 w-40">
--- src/modules/user/components/Register/RegisterForm.jsx
+++ src/modules/user/components/Register/RegisterForm.jsx
@@ -3,68 +3,129 @@ import { useState } from 'react';
 import registerStyle from"../../styles/Register.module.css"
 // import "../../styles/Register.css";
 
+const validationsForm = (form) => {
+  let errors = {};
+  let regexName = /^[A-Za-zÑñÁáÉéÍíÓóÚúÜü\s]+$/;
+  let regexEmail = /^(\w+[/./-]?){1,}@[a-z]+[/.]\w{2,}$/;
+
+  if(!form.name) {
+    errors.name = 'El campo "Nombre" es requerido'
+  } else if(!regexName.test(form.name.trim())){
+    errors.name = 'El campo "Nombre" solo acepta letras y espacios en blanco'
+  }
+
+  if(!form.lastName) {
+    errors.lastName = 'El campo "Apellido" es requerido'
+  } else if(!regexName.test(form.lastName.trim())){
+    errors.lastName = 'El campo "Apellido" solo acepta letras y espacios en blanco'
+  }
+
+  if(!form.phone) {
+    errors.phone = 'El campo "Teléfono" es requerido'
+  }
+
+  if(!form.email) {
+    errors.email = 'El campo "Email" es requerido'
+  } else if(!regexEmail.test(form.email.trim())){
+    errors.email = 'El campo "Email" es inconrrecto'
+  }
+
+  if(!form.pwd) {
+    errors.pwd = 'la contraseña es requerido'
+  }
+
+  if(!form.confirmarPassword) {
+    errors.confirmarPassword = 'Debe confirmar su contraseña'
+  }
+
+  return errors;
+}  
 
 function RegisterForm({ register }) {
   const [data, setData] = useState({ //setDate actualiza el objeto
-    nombre: '',
-    apellido: '',
-    telefono: '',
+    name: '',
+    lastName: '',
+    phone: '',
     email: '',
     pwd: '',
     confirmarPassword: ''
   })
 
-  const handleSubmit = e => { //evita el procesamiento automatico 
-    e.preventDefault()
-
-    register(data);
-  }
-
+  const [errors, setErrors] = useState({})
+//============================================================================
   const handleChange = (e) => {
+    const {name, value} = e.target
     setData ({
       ...data,
-      [e.target.name]: e.target.value
+      [name]: value
     });
+  }
+//============================================================================
+  const handleBlur = (e) => {
+    handleChange(e);
+    setErrors(validationsForm(data))
+  }
+//============================================================================
+  const handleSubmit = e => { //evita el procesamiento automatico 
+    e.preventDefault()
+    register(data);
   }
 
   return (
     <form className={registerStyle.container_form} onSubmit={handleSubmit} >
       <input type="text" 
-        name="nombre"
+        name="name"
         placeholder="Nombre"
         className={registerStyle.input_register}
         onChange={handleChange} //esta detectando el cambio del input
+        onBlur= {handleBlur}
       />
+      {errors.name && <p className={registerStyle.errorsRegister}>{errors.name}</p>}
+
       <input type="text" 
-        name="apellido"
+        name="lastName"
         placeholder="Apellido"
         className={registerStyle.input_register}
         onChange={handleChange}
+        onBlur= {handleBlur}
       />
+      {errors.lastName && <p className={registerStyle.errorsRegister}>{errors.lastName}</p>}
+
       <input type="number" 
-        name="telefono"
-        placeholder="telefono"
+        name="phone"
+        placeholder="Teléfono"
         className={registerStyle.input_register}
         onChange={handleChange}
+        onBlur= {handleBlur}
       />
+      {errors.phone && <p className={registerStyle.errorsRegister}>{errors.phone}</p>}
+
       <input type="email" 
         name="email"
         placeholder="Email"
         className={registerStyle.input_register}
         onChange={handleChange}
+        onBlur= {handleBlur}
       />
+      {errors.email && <p className={registerStyle.errorsRegister}> {errors.email} </p>}
+
       <input type="password" 
         name="pwd"
         placeholder="Password"
         className={registerStyle.input_register}
         onChange={handleChange}
+        onBlur= {handleBlur}
       /> 
+      {errors.pwd && <p className={registerStyle.errorsRegister}> {errors.pwd} </p>}
+
       <input type="password" 
         name="confirmarPassword"
         placeholder="Confirmar Password"
         className={registerStyle.input_register}
         onChange={handleChange}
+        onBlur= {handleBlur}
       />
+      {errors.confirmarPassword && <p className={`${registerStyle.errorsRegister} ${registerStyle.confpwd}` }> {errors.confirmarPassword} </p>}
       <button 
         type='submit'
         className="bg-indigo-600 text-white py-1 px-4 rounded hover:bg-indigo-800 duration-500 w-40">
